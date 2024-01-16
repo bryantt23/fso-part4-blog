1a2b9d5868767a004016c198b87c25671ac38034
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-16 03:45:51 -0800

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  return blogs.reduce((prev, cur) => {
+    const likes = cur.likes ? cur.likes : 0;
+    return prev + likes;
+  }, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
