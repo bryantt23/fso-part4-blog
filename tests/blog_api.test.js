2540969be06ccd091f7c7e52b073766e8c7cc5d7
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-16 23:12:14 -0800

    4.11*: Blog list tests, step4

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -55,6 +55,24 @@ test('a valid blog can be added', async () => {
   expect(titles).toContain('Test Blog');
 });
 
+test('likes property defaults to 0 if missing', async () => {
+  const newBlogWithoutLikes = {
+    title: 'Blog Without Likes',
+    author: 'No Liker',
+    url: 'http://nolikes.com'
+  };
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlogWithoutLikes)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  // Check that the likes property is defined and set to 0
+  expect(response.body.likes).toBeDefined();
+  expect(response.body.likes).toBe(0);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
