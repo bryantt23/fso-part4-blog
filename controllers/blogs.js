c7e6b75328e4abdb4fee71be0be531e989a61146
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-02-04 03:06:37 -0800

    fix put

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -78,7 +78,7 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
   const id = request.params.id;
-  const updatedBlog = await Blog.findByIdAndUpdate(id, request.body);
+  const updatedBlog = await Blog.findByIdAndUpdate(id, request.body, { new: true });
   response.status(200).json(updatedBlog);
 });
 
